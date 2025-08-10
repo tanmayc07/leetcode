f42b528e4f26c899a87cf016c9cf94ed852af68e
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2025-08-10 13:16:12 -0400

    Time: 0 ms (100%), Space: 55.1 MB (70.73%) - LeetHub

---
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.ts
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function inorderTraversal(root: TreeNode | null): number[] {
+    let tree: number[] = [];
+
+    function traverse(node: TreeNode | null) {
+        if(node !== null) {
+            traverse(node.left);
+            tree.push(node.val);
+            traverse(node.right);
+        }
+    }
+
+    traverse(root);
+    return tree;
+};
