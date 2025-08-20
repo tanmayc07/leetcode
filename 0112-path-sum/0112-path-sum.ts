9737e6f29cc39f6904230b6597b9f574362537c2
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2025-08-19 22:36:45 -0400

    Time: 8 ms (5.03%), Space: 60.5 MB (10.34%) - LeetHub

---
+++ 0112-path-sum/0112-path-sum.ts
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function hasPathSum(root: TreeNode | null, targetSum: number): boolean {
+    if(!root) return false;
+    
+    let st: [TreeNode, number][] = [];
+    let res: boolean = false;
+
+    st.push([root, root.val]);
+
+    while(st.length > 0) {
+        let [node, sm] = st.pop();
+
+        if(node.left === null && node.right === null && sm === targetSum) 
+            res = true;
+        else {
+            if(node.left !== null) st.push([node.left, sm+node.left.val]);
+            if(node.right !== null) st.push([node.right, sm+node.right.val]);
+        }
+    }
+
+    return res;
+};
