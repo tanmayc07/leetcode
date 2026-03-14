20a3e8cff5a96b620a934de630743b598dbc84fc
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2026-03-14 19:58:40 -0400

    Time: 6 ms (34.94%), Space: 58.7 MB (55.44%) - LeetHub

--- 0496-next-greater-element-i/0496-next-greater-element-i.ts
+++ 0496-next-greater-element-i/0496-next-greater-element-i.ts
@@ -1,3 +1,20 @@
 function nextGreaterElement(nums1: number[], nums2: number[]): number[] {
-    
+    let res = [];
+    let mp = new Map<number, number>();
+    let st = [];
+
+    for (let i=nums2.length-1; i>=0; i--) {
+        while (st.length>0 && st[st.length-1] <= nums2[i])
+            st.pop();
+        
+        if (st.length == 0) mp[nums2[i]] = -1;
+        else mp[nums2[i]] = st[st.length-1];
+
+        st.push(nums2[i]);
+    }
+
+    for (let i=0; i<nums1.length; i++) 
+        res.push(mp[nums1[i]]);
+
+    return res;
 };
