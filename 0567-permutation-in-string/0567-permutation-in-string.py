f6e55330f00f1046e730592771eb8ad400d6c391
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2026-03-15 17:57:32 -0400

    Time: 54 ms (23.81%), Space: 19.3 MB (95.6%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.py
+++ 0567-permutation-in-string/0567-permutation-in-string.py
@@ -1,20 +1,21 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
+        target = Counter(s1)
+        curr = Counter()
         left = 0
-        freq = Counter()
-        s1freq = Counter(s1)
 
         for right in range(len(s2)):
-            freq[s2[right]] += 1
-
-            if right-left+1 == len(s1) :
-                if freq == s1freq:
-                    return True
+            curr[s2[right]] += 1
             
-                freq[s2[left]] -= 1
-                if freq[s2[left]] == 0:
-                    del freq[s2[left]]
+            if right-left+1 == len(s1):
+                if target == curr:
+                    return True
+
+                curr[s2[left]] -= 1
+                if curr[s2[left]] == 0:
+                    del curr[s2[left]]
+
                 left += 1
-        
+
         return False
-                
+            
