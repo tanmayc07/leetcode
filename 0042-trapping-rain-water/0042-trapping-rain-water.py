3f3b7f3f160e252316341e3605c47350f25adcec
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2026-03-30 18:59:31 -0400

    Time: 15 ms (29.17%), Space: 21.1 MB (36.86%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,23 +1,22 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
-        n = len(height)
-        max_left = [0]*n
-        max_right = [0]*n
+        p_max = [0]*len(height)
+        s_max = [0]*len(height)
+        curr = p_max[0] = height[0] 
 
-        curr_max = 0
-        for i in range(n):
-            max_left[i] = curr_max
-            curr_max = max(curr_max, height[i])
+        for i in range(1, len(height)):
+            if height[i] > curr: 
+                curr = height[i]
+            p_max[i] = curr
         
-        curr_max = 0
-        for i in range(n-1,-1,-1):
-            max_right[i] = curr_max
-            curr_max = max(curr_max, height[i])
-
+        curr = s_max[-1] = height[-1] 
+        for i in range(len(height)-1, -1, -1):
+            if height[i] > curr:
+                curr = height[i]
+            s_max[i] = curr
+        
         total_trapped = 0
-        for i in range(n):
-            curr = min(max_left[i], max_right[i]) - height[i]
-            if curr >= 0:
-                total_trapped += curr
-
+        for i in range(len(height)):
+            total_trapped += min(p_max[i], s_max[i]) - height[i]
+        
         return total_trapped
