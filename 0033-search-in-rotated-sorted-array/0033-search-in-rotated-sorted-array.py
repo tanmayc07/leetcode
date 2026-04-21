8309a0392de6ff558d996c91c23ad0d636629ae6
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2026-04-21 17:51:53 -0400

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.py
@@ -9,7 +9,7 @@ class Solution:
                 if nums[left]<=target<nums[mid]: right = mid-1
                 else: left = mid+1
             else:
-                if nums[mid]<target<=right: left = mid+1
+                if nums[mid]<target<=nums[right]: left = mid+1
                 else: right = mid-1
         
         return -1
