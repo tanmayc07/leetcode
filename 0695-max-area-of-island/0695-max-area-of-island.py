d59bfe94c1bd65a75dec51f3d562f32549c1ad66
Tanmay Chaudhari <48885301+tanmayc07@users.noreply.github.com>
2026-08-16 20:55:35 -0400

    Time: 19 ms (61.59%), Space: 19.3 MB (95.73%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.py
+++ 0695-max-area-of-island/0695-max-area-of-island.py
@@ -1,30 +1,32 @@
 class Solution:
     def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        if not grid: return 0
+
         rows, cols = len(grid), len(grid[0])
         max_area = 0
 
-        def dfs(r: int, c: int, a: int) -> int:
-            if (
-                r<0 or r>=rows or c<0 or c>=cols or grid[r][c]==0
-            ):
-                return 0
-            
-            grid[r][c] = 0
-            a = 1
-            
-            # print(f"({r},{c},{a}) -> {a}")
-            a += dfs(r+1, c, a)
-            a += dfs(r-1, c, a)
-            a += dfs(r, c+1, a)
-            a += dfs(r, c-1, a)
+        def bfs(r, c, a):
+            q = deque([(r, c)])
+
+            while q:
+                row, col = q.popleft()
+                for dr, dc in [(1,0),(-1,0),(0,1),(0,-1)]:
+                    nr, nc = row+dr, col+dc
+
+                    if 0<=nr<rows and 0<=nc<cols and grid[nr][nc]!=0:
+                        grid[nr][nc] = 0 
+                        a += 1
+                        q.append((nr, nc))
 
             return a
+                
 
 
         for r in range(rows):
             for c in range(cols):
                 if grid[r][c] == 1:
-                    area = dfs(r, c, 0)
+                    grid[r][c] = 0
+                    area = bfs(r, c, 1)
                     max_area = max(max_area, area)
 
         return max_area
